d545d2a5bcbd53af7a624a5a8ca347af48cb9101
erik <erik@erikbot>
2015-07-28 15:22:22 -0500

    Integral

---
+++ ejercicioIntegral.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import math
+def areaTrapezio(espacio,y1,y2):
+	return  (y1+y2)*espacio/2
+	
+def f(x):
+	return math.sqrt(x)
+
+def calcularIntegral(xInicial,xFinal, div):
+	total  = 0 
+	espacio = (xFinal-xInicial)/div
+	contador = 0 
+	
+	while(contador < div):
+		tmp = xInicial
+		xInicial+=espacio
+		total += areaTrapezio(espacio , f(tmp),f(xInicial))		
+		contador = contador+1
+	print(total)
+
+x1 = input("ingrese el xinicial: ")
+x2 = input("ingrese el xfinal: ")
+d = input("ingrese cuantas veces quiere dividir el espacio: ")	
+	
+calcularIntegral(x1,x2,d)
+
